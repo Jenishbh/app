83e81b95d9721be852d57e83247eabe72de02df5
Jenish <Jenishpatel1999@gmail.com>
2023-11-28 17:04:35 -0800

    User can able to see thier order recipt

--- screens/Customer/RegistrationDetails.js
+++ screens/Customer/RegistrationDetails.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, FlatList, SafeAreaView  } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, FlatList, SafeAreaView, TouchableOpacity, Modal, Alert, Linking   } from 'react-native';
 import Icon from 'react-native-vector-icons/Fontisto';
 import Icona from 'react-native-vector-icons/Ionicons';
 import Ico from 'react-native-vector-icons/MaterialCommunityIcons';
 
 function RegistrationDetails({ route }) {
   const { reservation } = route.params;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [receiptUrl, setReceiptUrl] = useState('');
   const renderFoodItem = ({ item }) => (
     <View style={styles.foodItemContainer}>
       <Text style={styles.foodName}>{item.name}</Text>
@@ -13,7 +15,15 @@ function RegistrationDetails({ route }) {
       <Text style={styles.foodQuantity}>{`Qty: ${item.qty}`}</Text>
     </View>
   );
+  const openReceiptModal = (url) => {
+    setReceiptUrl(url); // Set the receipt URL from the reservation data
+    setModalVisible(true);
+  };
 
+  // Function to close the receipt modal
+  const closeReceiptModal = () => {
+    setModalVisible(false);
+  };
 
 
   return (
@@ -38,7 +48,28 @@ function RegistrationDetails({ route }) {
           keyExtractor={(item) => item.id.toString()}
           style={styles.foodList}
         />
-      
+      {/* Button to view the receipt */}
+      <TouchableOpacity onPress={()=>openReceiptModal(reservation.receiptImageUrl)} style={styles.receiptButton}>
+        <Text style={styles.receiptButtonText}>View Receipt</Text>
+      </TouchableOpacity>
+
+      {/* Modal for displaying the receipt */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeReceiptModal}
+      >
+        <View style={styles.modalView}>
+          <Image
+            source={{uri: receiptUrl}} // Replace with dynamic receipt image source
+            style={styles.receiptImage}
+          />
+          <TouchableOpacity onPress={closeReceiptModal} style={styles.closeButton}>
+            <Text style={styles.closeButtonText}>Back</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
       </View>
     </SafeAreaView>
   );
@@ -108,6 +139,39 @@ const styles = StyleSheet.create({
   },
   foodList: {
     marginVertical: 15,
+  },
+  receiptButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    margin: 20,
+    borderRadius: 20,
+    alignItems: 'center',
+  },
+  receiptButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  receiptImage: {
+    width: '90%',
+    height: '80%',
+    resizeMode: 'contain',
+    backgroundColor: '#fff',
+    marginBottom: 20,
+  },
+  closeButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
 
